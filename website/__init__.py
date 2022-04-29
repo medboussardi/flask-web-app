6116c9151a8327339e41847f12046a745c90c540
simo <mohammed.boussardi02@gmail.com>
2022-04-29 16:07:34 +0000

    templates

--- website/__init__.py
+++ website/__init__.py
@@ -1,4 +1,3 @@
-import imp
 from flask import Flask
 
 
@@ -10,6 +9,7 @@ def create_app():
     from .auth import auth
     
     app.register_blueprint(views,url_prefix='/')
-    auth.register_blueprint(views,url_prefix='/')
+    app.register_blueprint(auth,url_prefix='/')
+    
     return app
 
--- website/views.py
+++ website/views.py
@@ -1,9 +1,8 @@
-import imp
-from flask import Blueprint
+from flask import Blueprint,render_template
 
 views = Blueprint('views',__name__)
 
 @views.route('/')
 
 def home():
-    return "<h1> Helllo </h1>"
+    return render_template("home.html")
